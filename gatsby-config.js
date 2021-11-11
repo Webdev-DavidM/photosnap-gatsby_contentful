f423e62505bb5a1c058558348b28bebe7a6270dc
Webdev-DavidM <56081000+Webdev-DavidM@users.noreply.github.com>
2021-11-11 10:45:11 +0000

    header completed

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,35 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "photosnap",
+    siteUrl: 'https://www.yourdomain.tld',
+    title: 'photosnap',
   },
   plugins: [
     {
-      resolve: "gatsby-source-contentful",
+      resolve: 'gatsby-source-contentful',
       options: {
-        accessToken: "9cUCFaH5EBKCp3s10KCVvWSj4FQjQSPGCUJf-lY816c",
-        spaceId: "",
+        accessToken: '9cUCFaH5EBKCp3s10KCVvWSj4FQjQSPGCUJf-lY816c',
+        spaceId: 'ubkqr1o0w5v4',
       },
     },
-    "gatsby-plugin-sass",
+    'gatsby-plugin-sass',
+    'gatsby-plugin-image',
+    {
+      resolve: 'gatsby-plugin-react-svg',
+      options: {
+        rule: {
+          include: /assets/,
+        },
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `DM Sans`,
+          `source sans-serif\:400,700`, // you can also specify font weights and styles
+        ],
+        display: 'swap',
+      },
+    },
   ],
 };
---
+++ src/components/layout/Header.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import * as styles from './header.module.scss';
+import Logo from '../../assets/shared/mobile/logo.svg';
+import HamburgerMenu from '../../assets/shared/mobile/menu.svg';
+import CloseMenu from '../../assets/shared/mobile/close.svg';
+import { Link } from 'gatsby';
+
+export default function Header() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  let menu = !showMenu
+    ? styles.header
+    : `${styles.header} ${styles.header__show}`;
+
+  return (
+    <div className={menu}>
+      <div className={styles.header__logo_container}>
+        <Logo className={styles.header__logo} />
+        {showMenu && (
+          <button onClick={() => setShowMenu(false)}>
+            <CloseMenu className={styles.header__cross} />
+          </button>
+        )}
+        {!showMenu && (
+          <button onClick={() => setShowMenu(true)}>
+            <HamburgerMenu className={styles.header__hamburger} />
+          </button>
+        )}
+      </div>
+      <div className={styles.header__links}>
+        <Link to='/stories' className={styles.header__link}>
+          STORIES
+        </Link>
+        <Link to='/features' className={styles.header__link}>
+          FEATURES
+        </Link>
+        <Link to='/pricing' className={styles.header__link}>
+          PRICING
+        </Link>
+        <div className={styles.header__break}></div>
+      </div>
+      <button className={styles.header__button}>GET AN INVITE</button>
+    </div>
+  );
+}
