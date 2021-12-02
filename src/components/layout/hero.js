ce53a41cfd7faa618bebb7f16806b69cd7b3868d
Webdev-DavidM <56081000+Webdev-DavidM@users.noreply.github.com>
2021-12-02 17:46:58 +0000

    hero issue resolved

--- src/components/layout/hero.js
+++ src/components/layout/hero.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import '../../scss/hero.scss';
-import Media from 'react-media';
 import Arrow from '../../assets/shared/desktop/arrow-footer.svg';
 import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 
@@ -19,36 +18,18 @@ export default function hero({ props }) {
   const mobileImage = getImage(heroImageMobile);
   return (
     <div className='hero'>
-      {/* <Media
-        queries={{
-          mobile: '(max-width: 768px)',
-          tablet: '(min-width: 769px) and (max-width: 1023px)',
-          desktop: '(min-width: 1024px)',
-        }}>
-        {(matches) => (
-          <>
-            {matches.mobile && (
-              <GatsbyImage className='hero__image' image={mobileImage} />
-            )}
-            {matches.tablet && (
-              <GatsbyImage
-                className='hero__image'
-                image={tabletImage}
-                alt='background image'
-              />
-            )}
-            {matches.desktop && (
-              <GatsbyImage
-                className='hero__image'
-                image={desktopImage}
-                alt='background image'
-              />
-            )}
-          </>
-        )}
-      </Media> */}
       <GatsbyImage
-        className='hero__image'
+        className='hero__image hero__image--mobile'
+        image={mobileImage}
+        alt='background image'
+      />
+      <GatsbyImage
+        className='hero__image hero__image--tablet'
+        image={tabletImage}
+        alt='background image'
+      />
+      <GatsbyImage
+        className='hero__image hero__image--desktop'
         image={desktopImage}
         alt='background image'
       />
